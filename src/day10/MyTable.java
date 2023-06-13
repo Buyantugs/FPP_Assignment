117a908c8f33731e1fe93a8ba0a7a67a44fb08f3
togs2 <buyantugs.luu@gmail.com>
2023-06-13 14:46:32 -0500

    Merge remote-tracking branch 'origin/master'

--- src/day10/MyTable.java
+++ src/day10/MyTable.java
@@ -1,61 +1,64 @@
-package Assigment6;
-
-import weekend.Singleton;
-
+package day10;
 public class MyTable {
-    private Entry[] entries=new Entry[26];
-
-    //returns the String that is matched with char c in the table
-    public String get(char c){
-        //implement
-        int index = c - 'a';
-        return entries[index].str;
+    private Entry[] entries;
+    public MyTable() {
+        entries = new Entry[26];
     }
+    public String get(char c) {
 
-    //adds to the table a pair (c, s) so that s can be looked up using c
-    public void add(char c, String s) {
         //implement
-        int index = c - 'a';
-        if (index >= 0 && index < entries.length) {
-            entries[index] = new Entry(c, s);
-        }
-    }
-
-    //returns a String consisting of nicely formatted display
-    //of the contents of the table
-    @Override
-    public String toString() {
-        //implement
-        String ss="";
-
-        for (Entry e:entries) {
-            if (e != null) {
-                ss=ss+e.ch+"->"+e.str+'\n';
+        for (int i = 0; i < entries.length; i++) {
+            if (entries[i].ch == c) {
+                return entries[i].str;
             }
         }
-        return ss;
+        return null;
     }
-    private class Entry {
-        char ch;
-        String str;
-        Entry(char ch, String str){
-            this.ch=ch;
-            this.str=str;
-        }
-        //returns a String of the form "ch->str"
-        public String toString() {
-            return ch+"->"+str;
+
+    public void add(char c, String s) {
+
+        int idx = c - 'a';
+        if (idx >= 0 && idx < entries.length) {
+            entries[idx] = new Entry(c, s);
+
+            int index = c - 'a';
+            if (index >= 0 && index < entries.length) {
+                entries[index] = new Entry(c, s);
+
+            }
         }
     }
 
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+        for (Entry e:entries){
+            if (e!=null){
+                sb.append(e+"\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    class Entry {
+        char ch;
+        String str;
+
+        Entry(char ch, String str) {
+            //implement
+            this.ch = ch;
+            this.str = str;
+        }
+        public String toString() {
+               return ch + "->" + str;
+        }
+    }
     public static void main(String[] args) {
         MyTable t = new MyTable();
         t.add('a', "Andrew");
-        t.add('b',"Billy");
-        t.add('c',"Charlie");
-        String s = t.get('b');
-        System.out.println(s);
-        //print all table value
-        System.out.println(t.toString());
+        t.add('b', "Billy");
+        t.add('w', "Willie");
+        System.out.println(t.get('a'));
+        System.out.println(t);
     }
 }
